4e09ebfc4c03897e9f4392110e5bc03d89c33eb5
lucamandis89 <lucamandis@gmail.com>
2026-02-07 16:39:55 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Strategia semplice e sicura: Network First
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
